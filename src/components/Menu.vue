bf31fda5f2161d3287fc8beee9752261b97860ca
Edilson Junior <edilson99noslide@gmail.com>
2025-05-17 22:02:05 -0300

    First commit

---
+++ src/components/Menu.vue
@@ -0,0 +1,20 @@
+<script setup>
+  import { useDark, useToggle } from '@vueuse/core'
+
+  const isDark = useDark()
+  const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <div class="menu-component__menu">
+    <button @click="toggleDark()">Alterar tema</button>
+  </div>
+</template>
+
+<style scoped>
+.menu-component__menu {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
